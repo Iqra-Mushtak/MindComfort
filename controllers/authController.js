1c7e3408803587c0c565359e9964c185918de5d2
Iqra-Mushtak <iqramushtaq2563@gmail.com>
2026-02-04 21:48:13 +0500

    register client + mentor

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,8 +54,30 @@ exports.createModerator = async (req, res) => {
     }
 };
 
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password, role } = req.body;
 
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists." });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+            role
+        });
 
+    await newUser.save();
+
+    res.status(201).json({ message: "Account created successfully."});
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
 
 
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { createAdmin, createModerator } = require('../controllers/authController');
+const { createAdmin, createModerator, register } = require('../controllers/authController');
 const { adminOnly } = require('../middleware/authmiddleware');
 
 router.post('/setup-admin', createAdmin);
 router.post('/create-moderator', adminOnly, createModerator);
+router.post('/register', register);
 
-module.exports = router;
+module.exports = router; 
