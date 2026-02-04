3a4d2868803fcef221531b16fabdfadffa08d376
Iqra-Mushtak <iqramushtaq2563@gmail.com>
2026-02-04 16:23:08 +0500

    backend setup 2

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+const authRoutes = require('./routes/authRoutes'); 
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send("The MindComfort Backend is officially running!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB Connected Successfully');
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch(err => {
+        console.log('Database connection error:', err.message);
+    });
