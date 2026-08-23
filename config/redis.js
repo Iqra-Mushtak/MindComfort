83f49538d314845c0e131994f18d9d909461b582
Iqra-Mushtak <iqramushtaq2563@gmail.com>
2026-08-24 00:14:45 +0500

    docker and redis production setup

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,6 @@
 const Redis = require('ioredis');
 require('dotenv').config();
 
-// const redisClient = redis.createClient({
-//     url: 'redis://localhost:6379'
-
 const redisClient = new Redis({
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
@@ -13,6 +10,5 @@ const redisClient = new Redis({
 
 redisClient.on('connect', () => console.log('Successfully connected to Redis'));
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
-// console.log("Checking password from env:", process.env.REDIS_PASSWORD);
 
 module.exports = redisClient;
