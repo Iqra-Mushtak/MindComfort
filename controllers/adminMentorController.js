5b5a6c5c6ff11b95ef1b227aa8000a63298c80e7
Iqra-Mushtak <iqramushtaq2563@gmail.com>
2026-09-10 16:27:53 +0500

    env var name updated in controller

--- controllers/adminMentorController.js
+++ controllers/adminMentorController.js
@@ -265,11 +265,11 @@ exports.getMentorStats = async (req, res) => {
 const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3');
 
 const s3Client = new S3Client({
-  endpoint: process.env.B2_ENDPOINT,
-  region: process.env.B2_REGION,
+  endpoint: process.env.BACKBLAZE_ENDPOINT,
+  region: process.env.BACKBLAZE_REGION || 'us-east-005',
   credentials: {
-    accessKeyId: process.env.B2_APPLICATION_KEY_ID,
-    secretAccessKey: process.env.B2_APPLICATION_KEY,
+    accessKeyId: process.env.BACKBLAZE_APPLICATION_KEY_ID,
+    secretAccessKey: process.env.BACKBLAZE_APPLICATION_KEY,
   },
 });
 
